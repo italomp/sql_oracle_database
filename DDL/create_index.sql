fa6be9e3c936e46091dd7c5ae23696d316b4eefb
Italo Modesto <italo.pereira@ccc.ufcg.edu.br>
2021-02-27 15:11:54 -0300

    fixing extension of the 'drop_table_and_truncate_table' and commiting create_index.sql file

---
+++ DDL/create_index.sql
@@ -0,0 +1,6 @@
+/*índice é uma estrutura de acesso físico mais eficiente
+  O oracle já criar índice automaticamente para  PKs e 
+  colunas UNIQUE.
+*/
+
+CREATE INDEX nome_index ON tabela(atributo)
