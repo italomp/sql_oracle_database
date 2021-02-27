d1bf4959949137bcdff6ae6602327fb95716ebc2
Italo Modesto <italo.pereira@ccc.ufcg.edu.br>
2021-02-27 16:39:14 -0300

    warning about cascade clause

--- DDL/drop_table_and_truncate_table.sql
+++ DDL/drop_table_and_truncate_table.sql
@@ -1,4 +1,16 @@
-/* DROP AND TRUNCATE TABLE */
+/* DROP AND TRUNCATE TABLE 
+
+   Mesmo adicionando a regra de remoção CASCADE
+   à FK da tabela que a contém, preciso usar a 
+   cláusula CASCADE caso eu vá TRUNCAR a tabela
+   que contém a PK.
+
+   ex: empregado tem uma fk cod_proj que referencia
+       projeto. Essa fk tem uma regra de remoção 
+       associada a ela (CASCADE).
+       Mesmo assim, preciso usar a cláusula CASCADE
+       (ou outra como SET NULL)ao truncar a projeto.
+*/
 TRUNCATE TABLE empregado;
 DROP TABLE empregado;
 
